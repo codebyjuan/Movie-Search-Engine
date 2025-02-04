2529e5203d094df1e204ae2e488a4abbc08c60d5
Juan <jbellevue83022@gmail.com>
2025-02-03 22:40:30 -0600

    Added api image as well as edited how the release date appears

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,3 +1,5 @@
+import "../css/MovieCard.css";
+
 export default function MovieCard({ movie }) {
   function onFavorite() {
     console.log("clicked");
@@ -6,7 +8,10 @@ export default function MovieCard({ movie }) {
   return (
     <div className="movie-card">
       <div className="movie-poster">
-        <img src={movie.url} alt={movie.title} />
+        <img
+          src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+          alt={movie.title}
+        />
         <div className="movie-overlay">
           <button className="favorite-btn" onClick={onFavorite}>
             ♥
@@ -15,7 +20,7 @@ export default function MovieCard({ movie }) {
       </div>
       <div className="movie-info">
         <h3>{movie.title}</h3>
-        <p>{movie.release_date}</p>
+        <p>{movie.release_date.split("-")[0]}</p>
       </div>
     </div>
   );
