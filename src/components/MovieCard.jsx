5315b4afa6b80a32428f480f467ed065e5071239
Juan <jbellevue83022@gmail.com>
2025-02-05 15:35:54 -0600

    Added code for favorite functionality

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,8 +1,14 @@
 import "../css/MovieCard.css";
+import { useMovieContext } from "../contexts/MovieContext";
 
 export default function MovieCard({ movie }) {
-  function onFavorite() {
-    console.log("clicked");
+  const { isFavorite, addToFavorites, removeFromFavorites } = useMovieContext();
+  const favorite = isFavorite(movie.id);
+  function onFavorite(e) {
+    e.preventDefault();
+
+    if (favorite) removeFromFavorites(movie.id);
+    else addToFavorites(movie);
   }
 
   return (
@@ -13,7 +19,10 @@ export default function MovieCard({ movie }) {
           alt={movie.title}
         />
         <div className="movie-overlay">
-          <button className="favorite-btn" onClick={onFavorite}>
+          <button
+            className={`favorite-btn ${favorite ? "active" : ""}`}
+            onClick={onFavorite}
+          >
             ♥
           </button>
         </div>
